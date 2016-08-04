b15580b77d8062f1cfd71bca3dd4fc1a6ed30062
vats14aug <vatsal.smile@gmail.com>
2016-08-05 01:00:56 +0530

    Add files via upload

---
+++ Pankaj.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Pankaj
+{
+public static void main(String []agrs)
+{
+System.out.println("Enter two numbers");
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=a+b;
+System.out.println("Total sum is:"+c);
+}
+}
