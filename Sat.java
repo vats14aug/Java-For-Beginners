26314108f6fa103f4b114bc40e5d7b52873a18bb
vats14aug <vatsal.smile@gmail.com>
2016-08-05 01:25:25 +0530

    Create Sat.java

---
+++ Sat.java
@@ -0,0 +1,21 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+class Sat
+{
+public static void main(String []agrs) throws IOException
+{
+ 
+BufferedReader br= new BufferedReader( new InputStreamReader(System.in));
+System.out.println("Enter id,name,sal");
+int id=Integer.parseInt(br.readLine());
+
+String name=br.readLine();
+
+int sal=Integer.parseInt(br.readLine());
+
+System.out.println(" Id is"+id+ "\nname is"+name);
+System.out.println(" name is"+name);
+System.out.println(" sal is"+sal);
+}
+}
