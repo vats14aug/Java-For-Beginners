b3fdf8c621ea3c0afe9af666047e2ec777e22172
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-08-24 00:18:27 +0530

    Add files via upload

---
+++ BreakContinue.java
@@ -0,0 +1,17 @@
+class BreakContinue
+{
+public static void main(String []args)
+{
+for(int i=1;i<=10;i++)
+{
+if(i==5)
+break;
+System.out.println(i);
+}
+for(int i=1;i<-10;i++)
+{
+if(i==5)
+continue;
+System.out.println(i);
+}
+}}
