6525f053819f647059cda16c33ace5eb22cbd41d
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-09-03 02:39:00 +0530

    Create ClsEncapsulation.java

---
+++ ClsEncapsulation.java
@@ -0,0 +1,39 @@
+//ENCAPSULATION EXAMPLE
+class ClasSalary 
+{
+ private String salary="12000";
+
+public String getSalary(String role) {
+	if(role=="MANAGER") //only manager can view his salary
+		return salary;
+	else 
+		return("No access");
+}
+
+public void setSalary(String salary, String role) {
+	if(role=="ADMIN")
+	this.salary = salary;
+	else
+		System.out.println("no access");
+}
+
+
+}
+
+class ClsEncapsule
+{   public static void main(String []args)
+
+	{
+	ClasSalary cs=new ClasSalary();
+	
+	//System.out.println("Salary is"+cs.salary);
+	System.out.println(cs.getSalary("MANAGER")); //only manager can view his salary
+	System.out.println(cs.getSalary("Team Member")); 
+	System.out.println(cs.getSalary("Team Lead"));
+	cs.setSalary("15000","ADMIN");//only admin can change the salary
+	System.out.println(cs.getSalary("MANAGER"));
+	System.out.println(cs.getSalary("Team Member"));
+	System.out.println(cs.getSalary("Team Lead"));
+	
+	}
+}
