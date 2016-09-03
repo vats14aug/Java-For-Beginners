f8fa6f3628acebfa1d38033a55ed69f288eae81a
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-09-03 14:02:36 +0530

    Inhertiance with constructors

---
+++ ClasChild.java
@@ -0,0 +1,13 @@
+package com.inheritance;
+	
+public class ClasChild extends Clssamp {
+	public static void main(String []args)
+	{
+		
+	
+	//Clssamp obj1=new Clssamp();
+	Clssamp obj1=new Clssamp(45);
+	 
+ System.out.println( (obj1.getSum()));
+}
+}
---
+++ Clssamp.java
@@ -0,0 +1,22 @@
+package com.inheritance;
+
+public class Clssamp  
+{
+	 public Clssamp()
+	{
+		i=10;
+		x=25;
+	}
+	 public Clssamp(int a)
+		{
+			i=10;
+			x=a;
+		}
+	private int i;
+	private int x;
+	public int getSum()
+ {
+	return(i+x);
+ }
+}
+
