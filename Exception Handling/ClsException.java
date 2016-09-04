e234e48f33ab7ff2429ff7fd4c920165648cebfb
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-09-04 16:34:27 +0530

    Update ClsException.java

--- Exception Handling/ClsException.java
+++ Exception Handling/ClsException.java
@@ -2,7 +2,7 @@ package com.inheritance;
 
 public class ClsException {
 public static void main(String []args)
-{ int i=10;
+{ int i=0;
 	printline(i);
 }
  public static void printline(int a)
@@ -10,7 +10,7 @@ public static void main(String []args)
  { int z=0;
 	 try {
 	
-		 int x=20;
+		 int x=10;
         z=x/a;
         System.out.println("No error here");
 	 	} 
@@ -24,7 +24,21 @@ public static void main(String []args)
 		 System.out.println("I am finally");
 		 
 	 }
-	 System.out.println("Result is"+z);
+	 
+ 	 try {
+		 z=5/0;
+		 
+	} catch (Exception e) {
+		System.err.println("Got Exception : "+ e.getMessage());
+		z=25;
+		// TODO: handle exception
+	}
+ 	 finally{
+ 		System.out.println("I am 2 finally");
+		 
+ 	 }
+
+	 System.out.println("Result is   "+z);
 	 System.out.println("Clear");
 
   }
