de943976ba46a5d5c1948fade4c6fb2266146fe5
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-09-04 18:24:15 +0530

    Add files via upload

---
+++ Polymorphism/Clsusage1.java
@@ -0,0 +1,17 @@
+package com.inheritance;
+
+public class Clsusage1 {
+public static void main(String []args)
+{
+	//Clsshape obj=new Clsshape();
+	// Clsshape obj=new Clsrect();
+	Clsrect obj=new Clsrect();
+	//Clsrect obj=new Clsshape();
+	
+	//---------------
+	obj.printline();
+	System.out.println(obj.i);
+	obj.met1();
+}
+
+}
