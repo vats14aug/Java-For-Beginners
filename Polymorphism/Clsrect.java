d777216894ea446fef1d75277a767f473425c2f3
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-09-04 18:18:20 +0530

    Add files via upload

---
+++ Polymorphism/Clsrect.java
@@ -0,0 +1,16 @@
+package com.inheritance;
+
+public class Clsrect extends Clsshape{
+	/*public int i=10;
+	
+	public void printline()
+	{
+		System.out.println("I am rectangle");
+	}
+	*/
+	public void met1()
+	{
+		System.out.println("I am personal method of rectangle");
+	}
+
+}
