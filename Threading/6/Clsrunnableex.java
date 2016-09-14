5d7647445108d1b7942da43bc8382fc8e7ba010d
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-09-15 01:09:07 +0530

    Runnable implementation of threads

---
+++ Threading/6/Clsrunnableex.java
@@ -0,0 +1,11 @@
+
+public class Clsrunnableex implements Runnable {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i=0;i<20;i++)
+		System.out.println("____________I am in child");
+	}
+
+}
+		
